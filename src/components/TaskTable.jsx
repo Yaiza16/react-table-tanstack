8c4ca837bedd7832e220a5453b12f5974e92430d
Yaiza Vallejo Caro <yaizavallejo16@gmail.com>
2024-03-24 15:15:33 +0100

    simple table

---
+++ src/components/TaskTable.jsx
@@ -0,0 +1,61 @@
+import { Box } from '@chakra-ui/react';
+import { flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import DATA from '../data';
+import { useState } from 'react';
+
+const columns = [
+  {
+    accessorKey: 'task',
+    Header: 'Task',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: 'status',
+    Header: 'Status',
+    cell: (props) => <p>{props.getValue()?.name}</p>,
+  },
+  {
+    accessorKey: 'due',
+    Header: 'Due',
+    cell: (props) => <p>{props.getValue()?.toLocaleTimeString()}</p>,
+  },
+  {
+    accessorKey: 'notes',
+    Header: 'Notes',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+];
+
+const TaskTable = () => {
+  const [data, setData] = useState(DATA);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  console.log(table.getHeaderGroups());
+  return (
+    <Box className='table' w={table.getTotalSize()}>
+      {table.getHeaderGroups().map((headerGroup) => (
+        <Box className='tr' key={headerGroup.id}>
+          {headerGroup.headers.map((header) => (
+            <Box className='th' w={header.getSize()} key={header.id}>
+              {header.column.columnDef.Header}
+            </Box>
+          ))}
+        </Box>
+      ))}
+      {table.getRowModel().rows.map((row) => (
+        <Box className='tr' key={row.id}>
+          {row.getVisibleCells().map((cell) => (
+            <Box className='td' w={cell.column.getSize} key={cell.id}>
+              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+            </Box>
+          ))}
+        </Box>
+      ))}
+    </Box>
+  );
+};
+export default TaskTable;
